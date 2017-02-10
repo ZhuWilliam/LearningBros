2632c71a79572ce0687af61553a6eb6b64e87779
William Zhu <wbzhu@ucsd.edu>
2017-02-09 17:37:16 -0800

    added empty files, cleaned html

---
+++ public/js/script.js
@@ -0,0 +1,16 @@
+/*
+
+YO FILL IN TEN WORDS
+<br><br>
+1:<input type="text"></input><br>
+2:<input type="text"></input><br>
+3:<input type="text"></input><br>
+4:<input type="text"></input><br>
+5:<input type="text"></input><br>
+6:<input type="text"></input><br>
+7:<input type="text"></input><br>
+8:<input type="text"></input><br>
+9:<input type="text"></input><br>
+10:<input type="text"></input><br>
+
+*/
