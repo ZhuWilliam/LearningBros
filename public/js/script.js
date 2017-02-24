7a34cbf33ddd275ab2376f2d626a8a6595f449e1
William Zhu <wbzhu@ucsd.edu>
2017-02-23 20:04:12 -0800

    added upData to some other pages, improved pae9

--- public/js/script.js
+++ public/js/script.js
@@ -1,28 +1,3 @@
-/*
-YO FILL IN TEN WORDS
-<br><br>
-1:<input type="text"></input><br>
-2:<input type="text"></input><br>
-3:<input type="text"></input><br>
-4:<input type="text"></input><br>
-5:<input type="text"></input><br>
-6:<input type="text"></input><br>
-7:<input type="text"></input><br>
-8:<input type="text"></input><br>
-9:<input type="text"></input><br>
-10:<input type="text"></input><br>
-*/
-
-
-function stepThrough(step) {
-    /* TODO if user pick a step to skip to, change step to that */
-    
-    if(step == 0) {
-        document.getElementById("content").innerHTML();
-    }
-    
-};
-
 function upData(kw, nextpage) {
     stuff = {};
     stuff[kw] = $('#'+kw).val();
