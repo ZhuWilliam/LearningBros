7a08f81a5ff1942816cef60a5920b29da2344fce
William Zhu <wbzhu@ucsd.edu>
2017-02-23 21:23:06 -0800

    trying to get page2 to work

--- public/js/script.js
+++ public/js/script.js
@@ -10,3 +10,13 @@ function upData(kw, nextpage) {
                 window.location.href = "/" + nextpage;
             });
 };
+
+// so the above function changes the dir, but some pages have multiple inputs 
+function upDataNoRedir(kw) {
+    stuff = {};
+    stuff[kw] = $('#'+kw).val();
+    console.log(kw);
+    console.log(stuff[kw]);
+
+    $.post("/ux2update", { stuff: stuff })
+}
